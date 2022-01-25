2c867997ae7b428fbad85e9dcb8310d6057037d7
Kamil Marczak <54755354+kamil1marczak@users.noreply.github.com>
2022-01-25 09:40:34 +0100

    Added GitHub username to pipeline file.

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -33,15 +33,15 @@ class PipelineStack(core.Stack):
 
         ecr_repo = _ecr.Repository(self, "lambda_container_pipeline")
 
-        ecr_output_name = core.CfnOutput(self, 'ecr_repo_name', 
+        ecr_output_name = core.CfnOutput(self, 'ecr_repo_name',
                 value=ecr_repo.repository_name,
                 export_name="ecr-repo-name")
 
-        ecr_output_uri = core.CfnOutput(self, 'ecr_repo_uri', 
+        ecr_output_uri = core.CfnOutput(self, 'ecr_repo_uri',
                 value=ecr_repo.repository_uri,
                 export_name="ecr-repo-uri")
 
-        github_repo='YOUR_GITHUB_USERNAME/cdk_pipelines_demo'
+        github_repo='kamil1marczak/cdk_pipelines_demo'
 
         git_hub_commit = _pipelines.CodePipelineSource.git_hub(
                     github_repo,
@@ -53,7 +53,7 @@ class PipelineStack(core.Stack):
         pipeline = _pipelines.CodePipeline(self, "Container_Pipeline",
             synth=_pipelines.ShellStep("Synth",
                 input=git_hub_commit,
-                commands=["npm install -g aws-cdk && pip install -r requirements.txt", 
+                commands=["npm install -g aws-cdk && pip install -r requirements.txt",
                             "cdk synth",
                             "pytest unittests"]
                 )
@@ -72,10 +72,10 @@ class PipelineStack(core.Stack):
             env={
                 "IMAGE_TAG": image_tag,
                 "AWS_ACCOUNT_ID": self.account,
-                "IMAGE_REPO_NAME": ecr_repo.repository_uri     
+                "IMAGE_REPO_NAME": ecr_repo.repository_uri
             }
         )
-        
+
         lambda_function1 = ApplicationStageLambda1(self, 'Container-CDK-Pipeline-Lambda-Stage1')
         lambda_function_stage1 = pipeline.add_stage(lambda_function1, pre=[buildContainerProject])
 
@@ -85,8 +85,8 @@ class PipelineStack(core.Stack):
 
         pipeline.build_pipeline()
 
-        ecr_repo_actions = ["ecr:PutImage", 
-                            "ecr:BatchCheckLayerAvailability", 
+        ecr_repo_actions = ["ecr:PutImage",
+                            "ecr:BatchCheckLayerAvailability",
                             "ecr:CompleteLayerUpload",
                             "ecr:InitiateLayerUpload",
                             "ecr:UploadLayerPart"]
